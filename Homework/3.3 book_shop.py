6c7dc088d40d3030410483f612ecc2c2ad20a0b1
kirstyabhus <kabhulimhen@Gmail.com>
2020-07-25 22:37:48 +0100

    /

--- Homework/3.3 book_shop.py
+++ Homework/3.3 book_shop.py
@@ -1,12 +1,13 @@
 book_year = int(input("What year was the book written in?\n"))
 
 if book_year == 1800:
-    print("Eighteenth Century")
+    century = "Eighteenth Century"
+elif 1900 >= book_year >= 1801:
+    century = "Nineteenth Century"
+elif 1900 < book_year <= 1950:
+    century = "Twentieth Century"
 
-if 1900 >= book_year >= 1801:
-    print("Nineteenth Century")
+if book_year < 190
 
-if 1900 < book_year <= 1950:
-    print("Twentieth Century")
 
-if book_year
+print("{},{}".format(century, decade))
