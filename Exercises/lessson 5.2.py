aa4f5897aac5f33811008b11ab83aa71178efbe7
kabhu <kabulimhen@gmail.com>
2020-08-11 21:25:10 +0100

    First

---
+++ Exercises/lessson 5.2.py
@@ -0,0 +1,10 @@
+species = [{'species': 'zebra', 'name': 'Penelope'},
+{'species': 'penguin', 'name': 'Jenn'},
+{'species': 'elephant', 'name': 'Harris'},
+{'species': 'flamingo', 'name': 'Florence'},]
+
+for specie in species:
+    print(specie['species'])
+
+
+
---
+++ Testing Playground/file_testing.py
@@ -0,0 +1,13 @@
+
+#with open("todo_list.txt", "w") as todo: # "w" is write creates a new file/  overwrites the same file
+
+with open("todo_list.txt", "a") as todo: # "a" is append which means dding to whats been created
+    new = input("What do you need to do today?: ")
+    todo.write(new + "\n")
+    todo.close()
+
+with open("todo_list.txt", "r") as todo: # "r" is read which opens a file
+    print(todo.read())
+    # OR
+    # contents = todo.read()
+    # print(contents)
