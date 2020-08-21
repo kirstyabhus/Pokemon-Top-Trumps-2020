bb034bf2356a5842f7f940eef3929db395f41544
kabhu <kabulimhen@gmail.com>
2020-08-21 13:26:21 +0100

    First

--- Main Project/Top Trumps.py
+++ Main Project/Top Trumps.py
@@ -21,14 +21,64 @@ def random_pokemon():
 
 def run():
     # displays the stats for the users' card
-    my_pokemon = random_pokemon()
-    print('Your card:')
-    print('NAME      {}'.format(my_pokemon['name']))
-    print('ID        {}'.format(my_pokemon['id']))
-    print('WEIGHT    {}'.format(my_pokemon['height']))
-    print('HEIGHT    {}'.format(my_pokemon['weight']))
+    my_pokemon1 = random_pokemon()
+    my_pokemon2 = random_pokemon()
+    my_pokemon3 = random_pokemon()
+
+    if my_pokemon1 == my_pokemon2 or my_pokemon1 == my_pokemon3:
+        my_pokemon1 = random_pokemon()
+    elif my_pokemon2 == my_pokemon3 or my_pokemon2 == my_pokemon1:
+        my_pokemon2 = random_pokemon()
+    elif my_pokemon3 == my_pokemon1 or my_pokemon3 == my_pokemon2:
+        my_pokemon3 = random_pokemon()
+
+    print('Your cards:')
+    print('CARD 1')
+    print('NAME      {}'.format(my_pokemon1['name']))
+    print('ID        {}'.format(my_pokemon1['id']))
+    print('WEIGHT    {}'.format(my_pokemon1['height']))
+    print('HEIGHT    {}'.format(my_pokemon1['weight']))
+    print('')
+    print('CARD 2')
+    print('NAME      {}'.format(my_pokemon2['name']))
+    print('ID        {}'.format(my_pokemon2['id']))
+    print('WEIGHT    {}'.format(my_pokemon2['height']))
+    print('HEIGHT    {}'.format(my_pokemon2['weight']))
+    print('')
+    print('CARD 3')
+    print('NAME      {}'.format(my_pokemon3['name']))
+    print('ID        {}'.format(my_pokemon3['id']))
+    print('WEIGHT    {}'.format(my_pokemon3['height']))
+    print('HEIGHT    {}'.format(my_pokemon3['weight']))
     print('')
 
+    card_pick = int(input('Which card do you want to use? (1, 2 or 3) '))
+    print('')
+    if card_pick == 1:
+        my_pokemon = my_pokemon1
+        print('Your card:')
+        print('NAME      {}'.format(my_pokemon1['name']))
+        print('ID        {}'.format(my_pokemon1['id']))
+        print('WEIGHT    {}'.format(my_pokemon1['height']))
+        print('HEIGHT    {}'.format(my_pokemon1['weight']))
+    elif card_pick == 2:
+        my_pokemon = my_pokemon2
+        print('Your card:')
+        print('NAME      {}'.format(my_pokemon2['name']))
+        print('ID        {}'.format(my_pokemon2['id']))
+        print('WEIGHT    {}'.format(my_pokemon2['height']))
+        print('HEIGHT    {}'.format(my_pokemon2['weight']))
+    elif card_pick == 3:
+        my_pokemon = my_pokemon3
+        print('Your card:')
+        print('NAME      {}'.format(my_pokemon3['name']))
+        print('ID        {}'.format(my_pokemon3['id']))
+        print('WEIGHT    {}'.format(my_pokemon3['height']))
+        print('HEIGHT    {}'.format(my_pokemon3['weight']))
+    else:
+        card_pick = input('Which card do you want to use? (1, 2 or 3) ')
+
+    print('')
     stat_choice = input('Which stat do you want to use? (id, height or weight) ')
     print('')
 
