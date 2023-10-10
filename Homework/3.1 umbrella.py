efecf3fbc2094ceee5122c88a691d52674c459f0
Kirsty Abhulimhen <68130549+kirstyabhus@users.noreply.github.com>
2023-10-10 15:29:33 +0100

    Delete unneeded directory

--- Homework/3.1 umbrella.py
+++
@@ -1,6 +0,0 @@
-raining = input("Is it raining outside? (y/n)\n")
-
-if raining == "y":
-    print("Take an umbrella.")
-elif raining == "n":
-    print("You don't need an umbrella")
--- Homework/4.2 chocolate_shop.py
+++
@@ -1,9 +0,0 @@
-chocolates = {'white': 1.50, 'milk': 1.20, 'dark': 1.80, 'vegan': 2.00,}
-
-buy = input("What item would you like to buy?")
-print(" ")
-
-price = chocolates[buy]
-
-print(" ")
-print("The price is £{}".format(price))
